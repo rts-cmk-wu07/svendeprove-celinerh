822d0e973f64f1097702f0380ee6146b94f5cf19
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-14 15:12:08 +0100

    add style and toast container

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function Layout() {
   return (
-    <div>
-      <main>
+    <div className="h-screen">
+      <main className="h-screen">
         <Outlet />
       </main>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
