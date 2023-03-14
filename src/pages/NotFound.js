2b03955fddb0d6943f9059140ad1c188e24e0827
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-14 15:52:28 +0100

    add style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Heading from "../components/Heading";
 
 function NotFound() {
   return (
-    <>
-      <h1>404</h1>
-      <p>Whoops, page not found.</p>
-      <Link className="block px-4 py-2 border w-fit" to="/">
+    <div className="page">
+      <Heading title="404" />
+      <p className="text-primaryText my-10">Whoops, page not found.</p>
+      <Link
+        className="block px-4 py-2 border border-buttonText w-fit text-buttonText rounded-[10px]"
+        to="/"
+      >
         Return home
       </Link>
-    </>
+    </div>
   );
 }
 
