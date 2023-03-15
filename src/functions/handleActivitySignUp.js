7d4b154d1998abf830f38233d0c59f1e7f3c8a51
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 14:12:56 +0100

    handle activity signup

---
+++ src/functions/handleActivitySignUp.js
@@ -0,0 +1,18 @@
+const handleActivitySignUp = (userToken, userId, activityId) => {
+  return fetch(
+    `http://localhost:4000/api/v1/users/${userId}/activities/${activityId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    }
+  ).then((response) => {
+    if (!response.ok) {
+      throw Error("Could not fetch the data for that resource");
+    }
+    return response.json();
+  });
+};
+
+export default handleActivitySignUp;
