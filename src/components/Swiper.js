890028e410496f2268883ca2503d22aa56983df8
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 13:07:30 +0100

    rename hide scrollbar class

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,6 +1,6 @@
 function Swiper({ children }) {
   return (
-    <div className="overflow-y-scroll flex flex-col gap-8 snap-y pb-20 scrollbar-hide">
+    <div className="overflow-y-scroll flex flex-col gap-8 snap-y pb-20 hide-scrollbar">
       {children}
     </div>
   );
