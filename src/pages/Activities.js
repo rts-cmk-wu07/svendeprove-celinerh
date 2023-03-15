0a8f5298fbd2295193b8cac789f9ee03252c3acb
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 09:29:13 +0100

    implement activities page

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,10 +1,33 @@
+import ActivityCard from "../components/ActivityCard";
+import ErrorMessage from "../components/ErrorMessage";
 import Heading from "../components/Heading";
 import Navigation from "../components/Navigation";
+import Spinner from "../components/Spinner";
+import Swiper from "../components/Swiper";
+import useActivities from "../hooks/useActivities";
 
 function Activities() {
+  const { activities, isLoading, error } = useActivities();
+
   return (
-    <div className="page">
+    <div className="page flex flex-col gap-6">
       <Heading title="Aktiviteter" />
+      {error && <ErrorMessage />}
+      {isLoading && <Spinner />}
+      {activities && (
+        <Swiper>
+          {activities?.map((activity) => (
+            <ActivityCard
+              key={activity.id}
+              id={activity.id}
+              imageSource={activity?.asset?.url}
+              title={activity?.name}
+              minAge={activity?.minAge}
+              maxAge={activity?.maxAge}
+            />
+          ))}
+        </Swiper>
+      )}
       <Navigation />
     </div>
   );
