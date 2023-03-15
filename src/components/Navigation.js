8e49a84d1ad13a59ce43b27efb3e9dd11ff75bd6
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 09:25:34 +0100

    fix z index

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import { FiCalendar } from "react-icons/fi";
 
 function Navigation() {
   return (
-    <nav className="absolute bottom-0 left-0 w-full bg-navigationBackground px-6 py-3">
+    <nav className="absolute bottom-0 left-0 w-full bg-navigationBackground px-6 py-3 z-50">
       <ul className="flex justify-between text-medium">
         <li>
           <NavLink className="navLink" to="/aktiviteter">
