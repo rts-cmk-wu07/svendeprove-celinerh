f7bc5bbf99ecc429be2edd0dec080f2894216cde
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 08:42:34 +0100

    create welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 function Welcome() {
-  return <div>Welcome</div>;
+  return (
+    <div className="relative bg-splash h-screen bg-no-repeat bg-center bg-cover">
+      <div className="absolute top-2/4 mt-3">
+        <h1 className="flex flex-col pl-11">
+          <span className="uppercase font-Roboto text-logoSize1 text-transparent text-stroke-1">
+            Landrup
+          </span>
+          <span className="uppercase font-RacingSansOne text-logoSize2 text-logoText text-stroke-2">
+            Dans
+          </span>
+        </h1>
+        <div className="w-[242px] h-[14px] bg-logoLineBackground shadow-logoLine mt-4"></div>
+      </div>
+      <Link
+        className="button absolute left-1/2 bottom-0 mb-14 -translate-x-1/2"
+        to="/aktiviteter"
+      >
+        Kom i gang
+      </Link>
+    </div>
+  );
 }
 
 export default Welcome;
