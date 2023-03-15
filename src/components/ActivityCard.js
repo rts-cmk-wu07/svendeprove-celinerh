5731e3ec965cea7ac9da45699fbcc39c99bd8760
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-15 09:28:30 +0100

    create activity card

---
+++ src/components/ActivityCard.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function ActivityCard({ id, imageSource, title, minAge, maxAge }) {
+  return (
+    <Link
+      className="relative rounded-t-[39px] rounded-l-[39px] overflow-hidden h-[344px] flex-shrink-0 snap-start"
+      to={`/aktiviteter/${id}`}
+    >
+      <img
+        className="h-full w-full object-cover"
+        src={imageSource}
+        alt={title}
+        title={title}
+      />
+      <div className="absolute inset-x-0 bottom-0 p-6 bg-cardBackground bg-opacity-80 z-20 rounded-tr-[39px] text-small">
+        <p>{title}</p>
+        <p>
+          {minAge}-{maxAge} år
+        </p>
+      </div>
+    </Link>
+  );
+}
+
+export default ActivityCard;
