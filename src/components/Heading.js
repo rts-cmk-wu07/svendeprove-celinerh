f9e85087b15f3e82b903b252940f4257cc4edd1c
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-16 11:15:04 +0100

    fix styling on heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Heading({ title }) {
-  return <h1 className="text-large text-heading">{title}</h1>;
+function Heading({ title, className = "" }) {
+  return <h1 className={`text-large text-heading ${className}`}>{title}</h1>;
 }
 
 export default Heading;
