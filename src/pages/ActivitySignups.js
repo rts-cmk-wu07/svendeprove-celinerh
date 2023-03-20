e38bec7755ed1678760b6b8173fc5ff1463319d4
cellle <91531988+celinerh@users.noreply.github.com>
2023-03-20 09:20:34 +0100

    fix heading and navigate if activity doesnt exist

--- src/pages/ActivitySignups.js
+++ src/pages/ActivitySignups.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import Heading from "../components/Heading";
 import Navigation from "../components/Navigation";
@@ -6,12 +8,19 @@ import useActivity from "../features/activities/hooks/useActivity";
 import useRoster from "../features/calendar/hooks/useRoster";
 
 function ActivitySignups() {
-  const { activity } = useActivity();
+  const navigate = useNavigate();
+  const { activity, isLoading: activityIsLoading } = useActivity();
   const { roster, isLoading, error } = useRoster();
+
+  useEffect(() => {
+    if (!activity && !activityIsLoading) {
+      navigate("/404");
+    }
+  }, [activity, activityIsLoading, navigate]);
 
   return (
     <div className="page h-screen flex flex-col gap-6">
-      <Heading className="truncate" title={activity?.name ?? "Loading..."} />
+      <Heading className="leading-10" title={activity?.name ?? "Loading..."} />
       {error && <ErrorMessage />}
       {isLoading && <Spinner centered />}
       {roster && roster.length > 0 && (
